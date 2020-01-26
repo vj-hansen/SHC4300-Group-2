1924baec572c5dd121b0c4efa0fce4b3a67367d0
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2020-01-26 17:14:41 +0100

    Update mod_m.vhd

--- D2_uart/mod_m.vhd
+++ D2_uart/mod_m.vhd
@@ -1,16 +1,9 @@
 ----------------------------------------------------------------------------------
--- Engineer: 
--- 
--- Design Name: 
 -- Module Name: mod_m - Behavioral
--- Project Name: 
 -- Target Devices: 
-
 ----------------------------------------------------------------------------------
-
 -- Listing 4.11 Mod-m counter
--- this baud-rate generator will generate sampling ticks
-
+-- This baud-rate generator will generate sampling ticks
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -18,9 +11,10 @@ use ieee.numeric_std.all;
 
 entity mod_m is
     generic (
-        N: integer := 4; -- num of bits
-        M: integer := 326 ); -- mod-326 counter 
+        N: integer := 8; -- num of bits
+        M: integer := 10 ); -- mod-326 counter 
 
+-- Frequency = 16x the required baud rate (16x oversampling)
 -- 19200 bps * 16 = 307200 ticks/s
 -- 100 MHz / 307200 = 325.52 = 326
 
@@ -43,8 +37,7 @@ begin
         end if;
     end process;
     -- next-state logic
-    r_next <= (others => '0') when r_reg=(M-1) else
-        r_reg+1;
+    r_next <= (others => '0') when r_reg=(M-1) else r_reg+1;
     
     -- output logic
     q <= std_logic_vector(r_reg);
