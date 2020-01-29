6d4024c4e125a3d3895daf642073f6a2626483d8
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2020-01-29 10:02:12 +0100

    Update uart_TB.vhd

--- D2_uart/uart_TB.vhd
+++ D2_uart/uart_TB.vhd
@@ -40,13 +40,13 @@ begin
         rst <= '1';
         wait for clk_period*2;
         rst <= '0';
-        rx <= '0';
+        rx <= '0'; -- start bit = 0
         wait for bit_period;
         for i in 0 to 7 loop
-            rx <= rx_data(i);
+            rx <= rx_data(i); -- 8 data bits
             wait for bit_period;
         end loop;
-        rx <= '1';
+        rx <= '1'; -- stop bit = 1
         wait;
     end process;
 end Behavioral;
