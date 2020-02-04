e506c5b3e87f0103d7d687ac106c789546a23e14
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2020-02-04 20:35:38 +0100

    Update timer.vhd

--- CCW1_MusicBox/CCW1 MusicBox RAM/timer.vhd
+++ CCW1_MusicBox/CCW1 MusicBox RAM/timer.vhd
@@ -1,5 +1,5 @@
 ----------------------------------------------------------------------------------
--- 0.5sec Simple counter
+-- 0.5 sec Simple counter
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -8,19 +8,17 @@ use IEEE.NUMERIC_STD.ALL;
 entity timer is
     generic (
         N: integer := 17; -- num of bits from code converter
-        M: integer := 50000 -- mod-50k (count 50k pulses)
+        M: integer := 50000 -- mod-50k (count 50k pulses)  -- 50,000 = 500 us. 
+                -- We need 100 MHz * 0.5 sec = 50 M clock cycles
         ); 
-    port ( 
-        clk, rst:   in std_logic;
-        from_td_on: in std_logic;
-        to_td_done: out std_logic
-        );
+   
+    port ( clk, rst:   in std_logic;
+           from_td_on: in std_logic;
+           to_td_done: out std_logic );
 end timer;
 
 architecture arch of timer is
-
 signal r_reg, r_next: unsigned (N-1 downto 0):= (others => '0');
-
 begin
 
     -- register
@@ -39,5 +37,4 @@ begin
     
     -- output logic
     to_td_done <= '1' when r_reg = (M) else '0';
-
 end arch;
