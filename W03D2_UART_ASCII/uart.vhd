b909318e8ba1aa82aa49f736bbc68864b7b894b3
Deivy <38313372+deivyka@users.noreply.github.com>
2020-02-04 15:04:08 +0100

    Update and rename uart_rx.vhd to uart.vhd

--- W03D2_UART_ASCII/uart.vhd
+++ W03D2_UART_ASCII/uart.vhd
@@ -1,34 +1,33 @@
 ----------------------------------------------------------------------------------
--- Module Name: UART RX
--- Target Devices: Basys 3
-----------------------------------------------------------------------------------
 -- listing 7.1
 -- UART receiver
-
+----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_std.all;
 ------------------------------------------------------------
-entity uart_rx is
+entity uart is
     generic (
         DBIT: integer := 8; -- data bits
         SB_TICK: integer := 16 ); -- ticks for stop bits
 
     Port ( 
-        clk, rst, rx, s_tick: in std_logic;
+        clk, rst, rx, from_s_tick: in std_logic;
         rx_done_tick : out std_logic;
-        dout : out std_logic_vector(7 downto 0) );
-end uart_rx;
-------------------------------------------------------------
-architecture arch of uart_rx is
+        to_dout : out std_logic_vector(7 downto 0) );
+end uart;
+
+architecture arch of uart is
     type state_type is (idle, start, data, stop);
     signal state_reg, state_next : state_type; -- current and next state
     signal s_reg, s_next : unsigned(3 downto 0); -- keep track of sampling ticks and count to 7 in the 'start' state
     signal n_reg, n_next : unsigned(2 downto 0); -- keep track of data bits received in the 'data' state
     signal b_reg, b_next : std_logic_vector(7 downto 0); -- (deserializes rx) retrieved bits are shifted into and reassembled in the 'b' register
+
+----------------------------------------------------------------------------------
 begin
-------------------------------------------------------------
--- FSMD state and data registers
+
+-- State registers
     process(clk, rst) begin
         if rst = '1' then
             state_reg <= idle;
@@ -42,9 +41,9 @@ begin
             b_reg <= b_next;
         end if;
     end process;
-------------------------------------------------------------    
--- next-state logic and data path functional routing
-    process(state_reg, s_reg, n_reg, b_reg, s_tick, rx)
+
+-- next-state logic
+    process(state_reg, s_reg, n_reg, b_reg, from_s_tick, rx)
     begin
         state_next <= state_reg;
         s_next <= s_reg;
@@ -52,6 +51,7 @@ begin
         b_next <= b_reg;
         rx_done_tick <= '0';
         case state_reg is 
+------------------------------------------------------------                
             when idle =>
                 if rx='0' then
                     state_next <= start;
@@ -60,7 +60,7 @@ begin
                 end if;
 ------------------------------------------------------------                
             when start =>
-                if (s_tick = '1') then
+                if (from_s_tick = '1') then
                     if s_reg=7 then -- restart counter
                         state_next <= data;
                         s_next <= (others=>'0');
@@ -71,7 +71,7 @@ begin
                 end if;
 ------------------------------------------------------------                
             when data =>
-                if (s_tick = '1') then
+                if (from_s_tick = '1') then
                     if s_reg=15 then -- read RxD, feed its value to deserializer, restart counter
                         s_next <= (others => '0');
                         b_next <= rx & b_reg(7 downto 1); -- b = rx & (b >> 1)
@@ -86,7 +86,7 @@ begin
                 end if;
 ------------------------------------------------------------
             when stop =>
-                if (s_tick = '1') then
+                if (from_s_tick = '1') then
                     if s_reg=(SB_TICK-1) then
                         state_next <= idle;
                         rx_done_tick <= '1';
@@ -96,5 +96,7 @@ begin
                 end if;
         end case;
     end process;
-    dout <= b_reg;
+    
+    -- Output
+    to_dout <= b_reg;
 end arch;
