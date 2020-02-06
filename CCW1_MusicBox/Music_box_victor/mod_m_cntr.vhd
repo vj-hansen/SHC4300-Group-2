be2dee7315000d1211f25dc6039c8bc298269e1e
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2020-02-06 14:45:01 +0100

    Update mod_m_cntr.vhd

--- CCW1_MusicBox/Music_box_victor/mod_m_cntr.vhd
+++ CCW1_MusicBox/Music_box_victor/mod_m_cntr.vhd
@@ -1,3 +1,6 @@
+-- Send ticks to Toggle Flip-Flop
+-- Mod-m counter (Listing 4.11 in FPGA Prototyping by VHDL Examples, Pong P. Chu)
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all; 
@@ -6,8 +9,8 @@ entity mod_m_counter is
     generic ( N : integer := 18 );
     
     port (  clk, reset : in  STD_LOGIC;
-            from_m_in : in STD_LOGIC_VECTOR(N-1 downto 0);
-            to_t_in: out std_logic;
+            from_m_in : in STD_LOGIC_VECTOR(N-1 downto 0); -- note in
+            to_t_in: out std_logic; -- tick to toggle flip-flop
             q : out std_logic_vector(N-1 downto 0) );
 end mod_m_counter;
 
