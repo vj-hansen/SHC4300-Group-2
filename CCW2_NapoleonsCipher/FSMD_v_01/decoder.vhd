5e9b53c989121cc3c32e5cfaf3ffec32186b066c
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2020-02-21 15:44:46 +0100

    Update decoder.vhd

--- CCW2_NapoleonsCipher/FSMD_v_01/decoder.vhd
+++ CCW2_NapoleonsCipher/FSMD_v_01/decoder.vhd
@@ -1,27 +1,26 @@
---- not working
+--- not tested, not connected to top.vhd
 
 library IEEE;
+library std;
 use IEEE.STD_LOGIC_1164.ALL;
-
-entity decoder is
-    generic (key_txt : integer:= 2 ); -- key = 'C'
-    Port ( clk, reset : in std_logic;
-           cipher_in : in integer;
-           msg_out : out integer range 0 to 26 );
+use ieee.numeric_std.all;
+-----------------------------------------------
+entity decoder is  
+    Port ( cipher_in : in std_logic_vector(7 downto 0);
+           msg_out : out std_logic_vector(7 downto 0) );
 end decoder;
-
-architecture Behavioral of decoder is
-    signal tmp_in : integer; -- cipher in
+-----------------------------------------------
+architecture arch of decoder is
     signal dout : std_logic_vector(7 downto 0);
-    signal decrypt: integer; -- message out
-
+    signal m_n, c_n : unsigned(7 downto 0);
+    constant k_n : std_logic_vector(7 downto 0) := X"6a"; -- key = 'j', used for testing
+-----------------------------------------------
 begin
-    -- convert ascii to integer
-    converter_unit : entity work.converter(Behavioral) 
-        port map ( clk=>clk, reset=>reset, from_dout=>dout, msg_out=>tmp_in);
-    
-    process(tmp_in) begin
-        decrypt <= (25+key_txt-tmp_in) mod 26;
+    process(cipher_in) begin
+        if (cipher_in>=X"61" or cipher_in<=X"7a") then 
+                c_n <= unsigned(cipher_in);
+                m_n <= ((25+unsigned(k_n)-c_n) mod 26)+97;
+            end if;
     end process;
-    msg_out <= decrypt;
-end Behavioral;
+    msg_out <= std_logic_vector(m_n); -- decoded message
+end arch;
