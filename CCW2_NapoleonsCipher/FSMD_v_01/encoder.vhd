fa5d0ce17e8399b23030cc5de5372e4683ea36ed
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2020-02-21 15:46:01 +0100

    Update encoder.vhd

--- CCW2_NapoleonsCipher/FSMD_v_01/encoder.vhd
+++ CCW2_NapoleonsCipher/FSMD_v_01/encoder.vhd
@@ -10,15 +10,15 @@ end encoder;
 -----------------------------------------------
 architecture arch of encoder is
     signal dout : std_logic_vector(7 downto 0);
-    signal m_i, c_i : unsigned(7 downto 0);
-    constant k_i : std_logic_vector(7 downto 0) := X"6a"; -- key = 'j', used for testing
+    signal m_n, c_n : unsigned(7 downto 0);
+    constant k_n : std_logic_vector(7 downto 0) := X"6a"; -- key = 'j', used for testing
 -----------------------------------------------
 begin
     process(msg_in) begin
         if (msg_in>=X"61" or msg_in<=X"7a") then 
-                m_i <= unsigned(msg_in);
-                c_i <= ((25-m_i+unsigned(k_i)) mod 26)+97;
+                m_n <= unsigned(msg_in);
+                c_n <= ((25-m_n+unsigned(k_n)) mod 26)+97;
             end if;
     end process;
-    cipher_out <= std_logic_vector(c_i);
+    cipher_out <= std_logic_vector(c_n);
 end arch;
