73e63a8962e93a864d87f7757fe68cf77f52cafe
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2020-02-22 18:03:38 +0100

    Update top.vhd

--- CCW2_NapoleonsCipher/FSMD_v_01/top.vhd
+++ CCW2_NapoleonsCipher/FSMD_v_01/top.vhd
@@ -5,13 +5,15 @@ use IEEE.STD_LOGIC_1164.ALL;
 ----------------------------------------------------------
 entity top is
     port ( rx, clk, reset, mode : in STD_LOGIC;
-            c_i : out std_logic_vector(7 downto 0) );
+           c_n, m_n : out std_logic_vector(7 downto 0);
+           tx : out std_logic );
     end top;
 ----------------------------------------------------------
 architecture Behavioral of top is
-    signal tick, rx_done_tick, wr_en : std_logic; 
-    signal dout : std_logic_vector(7 downto 0);
-    signal k_i : std_logic_vector(7 downto 0);
+    signal tick, rx_done_tick, wr_en : std_logic;
+    signal tx_done_tick, tx_start : std_logic; 
+    signal dout, din : std_logic_vector(7 downto 0);
+    signal k_n : std_logic_vector(7 downto 0);
     signal abus : std_logic_vector(11 downto 0);
     signal ram_data : std_logic_vector(7 downto 0);
 begin 
@@ -33,6 +35,16 @@ begin
         port map ( clk=>clk, we=>wr_en, addr=>abus, wrbus=>dout, rdbus=>ram_data );
 -------------------------------------------------------------------	  
     encoder_unit: entity work.encoder(arch)
-        port map (msg_in=>ram_data, cipher_out=>c_i);
+        port map ( msg_in=>ram_data, cipher_out=>c_n );
+-------------------------------------------------------------------	  
+    decoder_unit: entity work.decoder(arch)
+        port map ( cipher_in=>ram_data, msg_out=>m_n );
+-------------------------------------------------------------------	 
+    uart_tx_unit: entity work.uart_tx(arch)
+        port map ( clk=>clk, reset=>reset, tx_start=>tx_start, s_tick=>tick,
+                   din=>din, tx_done_tick=>tx_done_tick, tx=>tx );
+-------------------------------------------------------------------	
+    --key_ROM_unit: entity work.key_block(arch)
+     --   port map ( clk=>clk, nxt=>nxt, clr=>clr, data=>k_n);
 -------------------------------------------------------------------	
 end Behavioral;
