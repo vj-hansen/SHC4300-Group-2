65591d60d12097599876df2ea983e15293199f34
Biplav Karna <biplavkarna@Biplavs-MacBook-Air.local>
2020-03-06 00:24:00 +0100

    -removed decode function, added logic to check a to z -added logic for taking spaces between, replacing scanf with getchar until /r or /n is received -added logic for non repeating of display message, added temp variable to save /r or /n when option e/d/q option is entered -tested and verified logic

--- CCW2_NapoleonsCipher/Software_implementation/NapoleonCipher_zybo.c
+++ CCW2_NapoleonsCipher/Software_implementation/NapoleonCipher_zybo.c
@@ -27,38 +27,25 @@
 * @param	Original text and Cipher text
 * @return	Cipher text
 ****************************************************************************/
-void encode(const char* plainText, char* encryptText) {
-    char key[CIPHER_KEY_LEN] = CIPHER_KEY;
-    int textCounter, keyCounter = 0;
-    for (textCounter = 0; plainText[textCounter] != '\0'; textCounter++) {
-	int ciph = ((25-plainText[textCounter]) + key[keyCounter]) % 26;
-       	ciph = ciph + 'a';  // convert into ASCII, A = 65, a = '97'
-	encryptText[textCounter] = (char)ciph;
-	keyCounter++;
-	keyCounter = (keyCounter > 22) ? 0 : keyCounter; // keyCounter = 0 if keyCounter > key length, else keyCounter = keyCounter
-    }
-    encryptText[textCounter] = '\0';
-    return;
+void encodeDecode(const char* plainText, char* encryptText) {
+	char key[CIPHER_KEY_LEN] = CIPHER_KEY;
+	int textCounter, keyCounter = 0;
+	for (textCounter = 0; plainText[textCounter] != '\0'; textCounter++) {
+		if(plainText[textCounter] == ' ')
+		{
+			encryptText[textCounter] = ' ';
+			continue;
+		}
+		int ciph = ((25-plainText[textCounter]) + key[keyCounter]) % 26;
+		ciph = ciph + 'a';  // convert into ASCII, A = 65, a = '97'
+		encryptText[textCounter] = (char)ciph;
+		keyCounter++;
+		keyCounter = (keyCounter > CIPHER_KEY_LEN-1) ? 0 : keyCounter; // keyCounter = 0 if keyCounter > key length, else keyCounter = keyCounter
+	}
+	encryptText[textCounter] = '\0';
+	return;
 }
 
-/***************************************************************************
-* Decrypt the encrypted text and return the original text
-* @param	Cipher text and original text
-* @return	Original text
-****************************************************************************/
-void decode(const char* encryptedText, char* plainText) {
-    char key[CIPHER_KEY_LEN] = CIPHER_KEY;
-    int textCounter, keyCounter = 0;
-    for (textCounter = 0; encryptedText[textCounter] != '\0'; textCounter++) {
-        int ciph = ((25 + key[keyCounter] - encryptedText[textCounter]) ) % 26;
-        ciph = ciph + 'a';  // convert into ASCII, A = 65, a = '97'
-	plainText[textCounter] = (char)ciph;
-	keyCounter++;
-	keyCounter = (keyCounter > 22) ? 0 : keyCounter;
-    }
-    plainText[textCounter] = '\0';
-    return;
-}
 
 /***************************************************************************
 * Main function to either decode or encode text
@@ -66,40 +53,79 @@ void decode(const char* encryptedText, char* plainText) {
 * @return	None
 ****************************************************************************/
 int main() {
-    char text[MAX_TEXT_SIZE] = {'\0'};
-    char convertedText[MAX_TEXT_SIZE] = {'\0'};
-    char selection = '\0';
-    while (1) {
-	    while (selection != 'e' && selection != 'd' && selection != 'q') {
-		printf("\n****Napoleon Cipher****\nPress 'e' for encrypt or 'd' for decrypt, 'q' for quit: "); // this prints twice after first selection
-		scanf("%c", &selection);
-	    }
-	    if (selection == 'q') {
-	    	print("\nFinished...");
-		return 0;
-	    }
-	    if (selection == 'e') { // - Encode
-		print("\r\nEnter the plain text:\r\n");
-		scanf("%s", text);
-		encode(text, convertedText);
-		print("\r\nThe plain text:\r\n");
-		print(text);
-		print("\r\nThe encrypted text:\r\n");
-		print(convertedText);
-		print("\r\n");
-	    }
-	    else if (selection == 'd') { // - Decode
-		print("\r\nEnter the encrypted text:\r\n");
-		scanf("%s", text);
-		decode(text, convertedText);
-		print("\r\nThe encrypted text:\r\n");
-		print(text);
-		print("\r\nThe plain text:\r\n");
-		print(convertedText);
-		print("\r\n");
-	    }
-	    selection = '\0';
-    }
-    return 0;
-    cleanup_platform();
+	char text[MAX_TEXT_SIZE] = {'\0'};
+	char convertedText[MAX_TEXT_SIZE] = {'\0'};
+	char selection = '\0';
+	while (1) {
+		char temp;
+		while (selection != 'e' && selection != 'd' && selection != 'q') {
+			printf("\n****Napoleon Cipher****\nPress 'e' for encrypt or 'd' for decrypt, 'q' for quit: "); // this prints twice after first selection
+			scanf("%c%c", &selection, &temp);
+		}
+		if (selection == 'q') {
+			print("\nFinished...");
+			return 0;
+		}
+		if (selection == 'e' || selection == 'd' ) { // - Encode
+			if (selection == 'e')
+			{
+				print("\r\nEnter the plain text:\r\n");
+			}
+			else
+			{
+				print("\r\nEnter the encrypted text:\r\n");
+			}
+
+			int i = 0;
+			while((temp = getchar()))
+			{
+				//printf("%c",temp);
+
+				if((temp < 'a' || temp > 'z' ) && temp != ' ' && temp != '\r' && temp != '\n')
+				{
+				   printf("Invalid character ignoring\r\n");
+				   continue;
+				}
+
+				if (temp == '\n' || temp == '\r')
+				{
+					text[i]='\0';
+					break;
+				}
+				text[i] = temp;
+				i++;
+				if ( i == MAX_TEXT_SIZE -2 )
+				{
+					printf("max size reached\r\n");
+					i++;
+					text[i]='\0';
+					break;
+				}
+			}
+			encodeDecode(text, convertedText);
+			if (selection == 'e')
+			{
+				print("\r\nThe plain text:\r\n");
+			}
+			else
+			{
+				print("\r\nThe encrypted text:\r\n");
+			}
+			print(text);
+			if (selection == 'e')
+			{
+				print("\r\nThe encrypted text:\r\n");
+			}
+			else
+			{
+				print("\r\nThe plain text:\r\n");
+			}
+			print(convertedText);
+			print("\r\n");
+		}
+		selection = '\0';
+	}
+	return 0;
+	cleanup_platform();
 }
+
