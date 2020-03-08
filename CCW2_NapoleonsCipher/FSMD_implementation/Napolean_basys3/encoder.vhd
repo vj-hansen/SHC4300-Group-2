f64003d3c602bde26f61b767a8b9ceb59bf32277
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2020-03-08 18:43:15 +0100

    Update encoder.vhd

--- CCW2_NapoleonsCipher/FSMD_implementation/Napolean_basys3/encoder.vhd
+++ CCW2_NapoleonsCipher/FSMD_implementation/Napolean_basys3/encoder.vhd
@@ -9,14 +9,11 @@ entity encode_decode is
             from_key_bus : in std_logic_vector(7 downto 0);
             to_ram_bus   : out std_logic_vector(7 downto 0));
 end encode_decode;
-
 -----------------------------------------------
 architecture arch of encode_decode is
-
------------------------------------------------
 begin    
-    to_ram_bus <= X"20" when from_rx_bus = X"20" else -- check for space
-        X"0D" when  from_rx_bus = X"0D"  or from_rx_bus = X"0A" else
-        std_logic_vector(((25 - unsigned(from_rx_bus) + unsigned(from_key_bus)) mod 26) +97) when (from_enc = '1') else
-        std_logic_vector(((25 + unsigned(from_key_bus) - unsigned(from_rx_bus)) mod 26) +97);
+    to_ram_bus <=   X"20" when from_rx_bus = X"20" else -- check for space
+                    X"0D" when  from_rx_bus = X"0D"  or from_rx_bus = X"0A" else
+                    std_logic_vector(((25 - unsigned(from_rx_bus) + unsigned(from_key_bus)) mod 26) +97) when (from_enc = '1') else
+                    std_logic_vector(((25 + unsigned(from_key_bus) - unsigned(from_rx_bus)) mod 26) +97);
 end arch;
