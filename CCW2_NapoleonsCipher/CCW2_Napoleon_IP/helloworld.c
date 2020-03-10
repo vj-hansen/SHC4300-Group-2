9b8fc6d3a1fbfb9483bca0f090d81f5e570c337b
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2020-03-10 22:40:01 +0100

    Update helloworld.c

--- CCW2_NapoleonsCipher/CCW2_Napoleon_IP/helloworld.c
+++ CCW2_NapoleonsCipher/CCW2_Napoleon_IP/helloworld.c
@@ -7,8 +7,7 @@
 #include <string.h>
 
 #define MAX_TEXT_SIZE 200
-unsigned int *ipCiphAddr = XPAR_ENCODER_DECODER_0_ENCODER_DECODER_BASEADDR;
-
+unsigned int *ipCiphAddr = XPAR_ENCODER_DECODER_0_ENCODER_DECODER_BASEADDR; // pointer to base address
 
 int main() {
     char key_out[] ="jeanjacquesrousseau";
@@ -28,7 +27,7 @@ int main() {
     		return 0;
     	}
     	if (selection == 'e') {
-    		print("\r\nEnter the plain text:\r\n");
+    		print("\r\nEnter text:\r\n");
     		int i = 0;
     		while((temp = getchar())) {
     			if((temp < 'a' || temp > 'z' ) && temp != ' ' && temp != '\r' && temp != '\n') {
@@ -46,18 +45,21 @@ int main() {
     			}
     			i++;
     		}
+            // generate new key based on length of input-text
     		for (int u=0, v=0; u < strlen(text); ++u, ++v) {
-    			if (v == strlen(key_out)) {
-    				v = 0;
+    			if (v == strlen(key_out)) { 
+    				v = 0; // "restart" the key
     			}
     			new_key[u] = key_out[v];
     		}
-
+            
+            // Interaction with our IP block
     		for (int x=0; x < strlen(text); ++x) {
-    			*(ipCiphAddr + 0) = text[x]; 		// put text into slv_reg0
-    			*(ipCiphAddr + 2) = new_key[x]; 	// put key into slv_reg2
+    			*(ipCiphAddr + 0) = text[x]; 		    // put text into slv_reg0
+    			*(ipCiphAddr + 2) = new_key[x]; 	    // put key into slv_reg2
     			convertedText[x] = *(ipCiphAddr + 1); 	// read cipher text from slv_reg1
     		}
+            
     		printf("text in: %s\r\n", text);
     		printf("text out: %s\r\n", convertedText);
     		print("\r\n");
