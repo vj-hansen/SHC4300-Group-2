455109d1abb717c7d964343383ae4709e3a8c82c
Victor Hansen <0vjhansen@gmail.com>
2020-04-13 12:53:26 +0200

    a

--- CCW4/array_sort.c
+++ CCW4/array_sort.c
@@ -6,26 +6,28 @@
 
 int main() {
 	int i, j, a, n;
-        uint8_t number[30];
-        printf("Enter size of array: ");
-        scanf("%d", &n);
- 
-        printf("Enter numbers: \n");
-        for (i=0; i<n; ++i) {
-            scanf("%d", &number[i]);
-        }
-        for (i=0; i<n; ++i) {
-            for (j=i+1; j<n; ++j) {
-                if (number[i] > number[j]) {  // if n[0] > n[1], e.g. 10 > 9
-                    a = number[i]; // hold n[0], a = 10
-                    number[i] = number[j]; // swap n[1] into n[0], n[0] = 9
-                    number[j] = a; // assign , n[1] = 10
-                }
-            }
-        }
-        printf("Sorted: \n");
-        for (i = 0; i < n; ++i) {
-            printf("%d ", number[i]);
-        }
-       	printf("\n");
+  uint8_t number[30];
+  printf("Enter size of array: ");
+  scanf("%d", &n);
+  printf("Enter numbers: \n");
+  for (i=0; i<n; ++i) {
+    scanf("%d", &number[i]);
+  }
+
+  for (i=0; i<n; ++i) {
+    for (j=i+1; j<n; ++j) {
+      if (number[i] > number[j]) {
+        a = number[i];
+        number[i] = number[j];
+        number[j] = a;
+      }
     }
+  }
+
+  printf("Sorted: \n");
+  for (i = 0; i < n; ++i) {
+    printf("%d ", number[i]);
+  }
+  
+  printf("\n");
+}
