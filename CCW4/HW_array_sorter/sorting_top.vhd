cddf65f1dd05783db24135b384915598e9818efb
Victor Hansen <46825640+vjhansen@users.noreply.github.com>
2020-04-19 00:00:33 +0200

    Update sorting_top.vhd

--- CCW4/HW_array_sorter/sorting_top.vhd
+++ CCW4/HW_array_sorter/sorting_top.vhd
@@ -2,7 +2,7 @@
 -- Group 2: V. Hansen, B. Karna, D. Kazokas, L. Mozaffari
 -- Sort Top
 
--- store cell_data(data_elements-1) in an array to get sorted data.
+-- store data(num_cells-1) in an array to get sorted data.
 
 -- Based on:
 -- https://hackaday.com/2016/01/20/a-linear-time-sorting-algorithm-for-fpgas/
@@ -14,76 +14,75 @@ use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 -------------------------------------------------
 entity sort_top is
-    generic ( data_elements : integer := 10;
-              data_width    : integer := 8 ); -- 8-bit data elements
+    generic ( num_cells  : integer := 10;
+              data_width : integer := 8 );
     
-    port ( clk, rst     : in std_logic;
-           new_data     : in std_logic_vector(data_width-1 downto 0); -- unsorted data
-           sorted_array : out std_logic_vector(data_width-1 downto 0) ); 
+    port ( clk, rst    : in std_logic;
+           unsrtd_data : in std_logic_vector(data_width-1 downto 0);
+           srtd_data   : out std_logic_vector(data_width-1 downto 0) ); 
 end sort_top;
 -------------------------------------------------
 architecture behavioral of sort_top is
     component sorting_cell
         generic (data_width : integer := 8);
         
-        port ( clk, rst             : in std_logic;
-               new_data             : in std_logic_vector(data_width-1 downto 0);
-               prev_cell_data       : in std_logic_vector(data_width-1 downto 0);
-               prev_cell_occupied   : in boolean;
-               prev_cell_push       : in boolean;
-               next_cell_data       : out std_logic_vector(data_width-1 downto 0);
-               next_cell_occupied   : out boolean;
-               next_cell_push       : out boolean);
+        port ( clk, rst           : in std_logic;
+               unsrtd_data        : in std_logic_vector(data_width-1 downto 0);
+               pre_data           : in std_logic_vector(data_width-1 downto 0);
+               pre_full, pre_push : in boolean;
+               nxt_data           : out std_logic_vector(data_width-1 downto 0);
+               nxt_full, nxt_push : out boolean );
     end component;
 -------------------------------------------------
-    type data_bus       is array(0 to data_elements-1) of std_logic_vector(data_width-1 downto 0);
-    type occupied_bus   is array(0 to data_elements-2) of boolean;
-    type push_bus       is array(0 to data_elements-2) of boolean;
+    type data_arr   is array(0 to num_cells-1) of std_logic_vector(data_width-1 downto 0);
+    type full_arr   is array(0 to num_cells-2) of boolean;
+    type push_arr   is array(0 to num_cells-2) of boolean;
 -------------------------------------------------
-    signal cell_data     : data_bus;
-    signal cell_occupied : occupied_bus;
-    signal cell_push     : push_bus;
+    signal data     : data_arr; -- cell data
+    signal full     : full_arr; -- full/occupied cell
+    signal push     : push_arr;
 -------------------------------------------------
 begin
-    sorted_data : for cell in 0 to data_elements-1 generate
+    sorting_data : for n in 0 to num_cells-1 generate
         -- First cell:
-        -- prev_cell_data doesn't exist
-        first_cell : if cell = 0 generate
+        -- pre_data doesn't exist (since this is the first cell)
+        first_cell : if n = 0 generate
             begin first_cell : sorting_cell 
-                port map ( clk=>clk, rst=>rst, new_data=>new_data,
-                           prev_cell_data=>(others=>'0'),
-                           prev_cell_occupied=>true,
-                           prev_cell_push=>false,
-                           next_cell_data=>cell_data(cell),
-                           next_cell_occupied=>cell_occupied(cell),
-                           next_cell_push=>cell_push(cell) );
+                port map ( clk=>clk, rst=>rst, unsrtd_data=>unsrtd_data,
+                           pre_data=>(others=>'0'),
+                           pre_full=>true,
+                           pre_push=>false,
+                           nxt_data=>data(n),
+                           nxt_full=>full(n),
+                           nxt_push=>push(n) );
         end generate first_cell;
 --------------------------------------
         -- Last cell:
-        -- Don't connect the `next_cell_data` since we're at the last cell
-        last_cell : if cell = data_elements-1 generate
+        -- Don't connect the `nxt_data` since we're at the last cell
+        last_cell : if n = num_cells-1 generate
             begin last_cell : sorting_cell 
-                port map ( clk=>clk, rst=>rst, new_data=>new_data,
-                           prev_cell_data=>cell_data(cell-1),
-                           prev_cell_occupied=>cell_occupied(cell-1),
-                           prev_cell_push=>cell_push(cell-1),
-                           next_cell_data=>cell_data(cell) );
+                port map ( clk=>clk, rst=>rst, unsrtd_data=>unsrtd_data,
+                           pre_data=>data(n-1),
+                           pre_full=>full(n-1),
+                           pre_push=>push(n-1),
+                           nxt_data=>data(n) );
         end generate last_cell;
 --------------------------------------
         -- Regular cells (not first or last cell):
-        -- Connect `prev_cell` to the previous cell's `next_cell` values
-        -- Connect `next_cell` to the next cell's `prev_cell` values.
-        regular_cells : if (cell/=0) AND (cell/=data_elements-1) generate
+        -- Connect `pre_` to the previous cell's `nxt_` values
+        -- Connect `nxt_` to the next cell's `pre_` values.
+        regular_cells : if (n/=0) AND (n/=num_cells-1) generate
             begin regular_cells : sorting_cell 
-                port map ( clk=>clk, rst=>rst, new_data=>new_data,
-                           prev_cell_data=>cell_data(cell-1),
-                           prev_cell_occupied=>cell_occupied(cell-1),
-                           prev_cell_push=>cell_push(cell-1),
-                           next_cell_data=>cell_data(cell),
-                           next_cell_occupied=>cell_occupied(cell),
-                           next_cell_push=>cell_push(cell) );
+                port map ( clk=>clk, rst=>rst, 
+                           unsrtd_data=>unsrtd_data,
+                           pre_data=>data(n-1),
+                           pre_full=>full(n-1),
+                           pre_push=>push(n-1),
+                           nxt_data=>data(n),
+                           nxt_full=>full(n),
+                           nxt_push=>push(n) );
             end generate regular_cells;
-    end generate sorted_data;
+    end generate sorting_data;
 --------------------------------------
-    sorted_array <= cell_data(data_elements-1); -- use RAM for this?
+    srtd_data <= data(num_cells-1); -- sorted data is shown in descending order
 end behavioral;
