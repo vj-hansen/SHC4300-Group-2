45903881e6db17da7873bce31cf077cdbfcfc82b
Victor Hansen <46825640+vjhansen@users.noreply.github.com>
2020-04-19 00:03:32 +0200

    Update sorting.vhd

--- CCW4/HW_array_sorter/sorting.vhd
+++ CCW4/HW_array_sorter/sorting.vhd
@@ -9,43 +9,42 @@ use ieee.std_logic_unsigned.all;
 --------------------------------------
 entity sorting_cell is
     generic ( data_width : integer := 8 );
-    port ( clk, rst : in std_logic;
-           new_data : in std_logic_vector(data_width-1 downto 0); -- unsorted data
-           prev_cell_data     : in std_logic_vector(data_width-1 downto 0);
-           prev_cell_occupied : in boolean;
-           prev_cell_push     : in boolean;
-           next_cell_data     : out std_logic_vector(data_width-1 downto 0);
-           next_cell_occupied : out boolean;
-           next_cell_push     : out boolean);
+    
+    port ( clk, rst           : in std_logic;
+           unsrtd_data        : in std_logic_vector(data_width-1 downto 0);
+           pre_data           : in std_logic_vector(data_width-1 downto 0);
+           pre_full, pre_push : in boolean;
+           nxt_data           : out std_logic_vector(data_width-1 downto 0);
+           nxt_full, nxt_push : out boolean );
 end sorting_cell;
 --------------------------------------
-architecture behavioral of sorting_cell is
-    signal occupied         : boolean := false;
-    signal current_data     : std_logic_vector(data_width-1 downto 0) := (others=>'0');
-    signal can_accept_data  : boolean := false;
+architecture arch of sorting_cell is
+    signal full        : boolean := false;
+    signal crrnt_data  : std_logic_vector(data_width-1 downto 0) := (others=>'0');
+    signal accept_data : boolean := false;
 --------------------------------------
 begin
-    can_accept_data <= (new_data < current_data) OR NOT occupied;
-    next_cell_data <= current_data;
-    next_cell_occupied <= true when occupied else false;
-    next_cell_push <= true when (can_accept_data and occupied) else false;
+    accept_data <= (unsrtd_data < crrnt_data) OR NOT full;
+    nxt_data    <= crrnt_data;
+    nxt_full    <= true when (full) else false;
+    nxt_push    <= true when (accept_data AND full) else false;
 --------------------------------------
     process (clk) begin
         if rising_edge(clk) then
             if (rst = '1') then
-                occupied <= false;
+                full <= false;
             else
-                case occupied is
+                case full is
                     when false =>
-                        if (prev_cell_push) then
-                            occupied <= true;
-                        elsif (NOT prev_cell_push AND prev_cell_occupied) then
-                            occupied <= true;
+                        if (pre_push) then
+                            full <= true;
+                        elsif (NOT pre_push AND pre_full) then
+                            full <= true;
                         else
-                            occupied <= false;
+                            full <= false;
                         end if;
                     when true =>
-                        occupied <= true;
+                        full <= true;
                 end case;
             end if;
         end if;
@@ -54,16 +53,16 @@ begin
     process (clk) begin
         if rising_edge(clk) then
             if (rst = '1') then
-                current_data <= (others=>'0');
+                crrnt_data <= (others=>'0');
             else
-                if (prev_cell_push) then
-                    current_data <= prev_cell_data;
-                elsif (can_accept_data AND NOT prev_cell_push AND occupied) then
-                    current_data <= new_data;
-                elsif (NOT prev_cell_push AND NOT occupied AND prev_cell_occupied) then
-                    current_data <= new_data;
+                if (pre_push) then
+                    crrnt_data <= pre_data;
+                elsif (accept_data AND NOT pre_push AND full) then
+                    crrnt_data <= unsrtd_data;
+                elsif (NOT pre_push AND NOT full AND pre_full) then
+                    crrnt_data <= unsrtd_data;
                 end if;
             end if;
         end if;
     end process;
-end behavioral;
+end arch;
