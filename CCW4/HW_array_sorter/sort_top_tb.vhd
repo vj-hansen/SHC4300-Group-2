112a76dfa677c2cde0b700a263de2ce55af693bc
Victor Hansen <46825640+vjhansen@users.noreply.github.com>
2020-04-19 00:09:03 +0200

    Update sort_top_tb.vhd

--- CCW4/HW_array_sorter/sort_top_tb.vhd
+++ CCW4/HW_array_sorter/sort_top_tb.vhd
@@ -1,7 +1,6 @@
 ----------------------------------
 -- Array Sorter - TestBench
 ----------------------------------
-library std;
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
