45ec64e6fe23a566f86ddfb84af057b1b59046f9
Victor Hansen <46825640+vjhansen@users.noreply.github.com>
2020-04-19 23:23:28 +0200

    Update sorting.vhd

--- CCW4/HW_array_sorter/sorting.vhd
+++ CCW4/HW_array_sorter/sorting.vhd
@@ -1,6 +1,10 @@
---------------------------------------
 -- Group 2: V. Hansen, B. Karna, D. Kazokas, L. Mozaffari
 -- Sorting Cell
+-- * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+-- Based on:
+    -- https://hackaday.com/2016/01/20/a-linear-time-sorting-algorithm-for-fpgas/
+    -- Sorting Units for FPGA-Based Embedded Systems, R. Marcelino, H. Neto, and J. M. P. Cardoso, 2008
+-- * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 --------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
