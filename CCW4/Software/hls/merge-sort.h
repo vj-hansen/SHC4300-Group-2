a5ce7bae9dca2e072babd3e2b655ca57d450412a
Biplav Karna <biplavkarna@Biplavs-MacBook-Air.local>
2020-04-21 17:27:52 +0200

    hls code, merge sort in iterative as, recursive is not supported for IP block

---
+++ CCW4/Software/hls/merge-sort.h
@@ -0,0 +1,5 @@
+#define MAX_NUMBERS 200
+#define min(x,y) x>y?y:x
+void mergeSort(int inputNums[MAX_NUMBERS], int start, int end);
+void mergeJoin(int inputNums[MAX_NUMBERS], int start,int mid, int end);
+
