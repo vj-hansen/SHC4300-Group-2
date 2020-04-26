c827dfd39c4141603e43de6507c028c980a5739d
Victor Hansen <46825640+vjhansen@users.noreply.github.com>
2020-04-26 14:28:57 +0200

    Update sort_top_tb.vhd

--- CCW4/HW_array_sorter/sort_read_ROM/sort_top_tb.vhd
+++ CCW4/HW_array_sorter/sort_read_ROM/sort_top_tb.vhd
@@ -6,37 +6,31 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 ----------------------------------
-entity sort_top_tb is
-end sort_top_tb;
+entity main_top_tb is
+end main_top_tb;
 ----------------------------------
-architecture arch of sort_top_tb is
-    component sort_top
-        generic ( data_width : integer := 8);
-                  
-        port ( clk, rst    : in std_logic;
+architecture arch of main_top_tb is
+    component main_top is    
+        port ( clk, rst       : in std_logic;
                to_clr_data_in : in std_logic;
                to_inc_data_in : in std_logic;
-               ram_wr : in std_logic;
-              -- unsrtd_data : in std_logic_vector(data_width-1 downto 0);
-               srtd_data   : out std_logic_vector(data_width-1 downto 0));
+               ram_wr         : in std_logic );
     end component;
 ----------------------------------
-    constant clk_period : time := 10 ns;
-    constant data_width : integer := 8;
-    signal clk, rst     : std_logic := '0';
-   -- signal unsrtd_data  : std_logic_vector(data_width-1 downto 0) := (others => '0');
-    signal srtd_data    : std_logic_vector(data_width-1 downto 0) := (others => '0');
+    constant clk_period     : time := 10 ns;
+    constant data_width     : integer := 8;
+    signal clk, rst         : std_logic := '0';
+    signal sorted_data      : std_logic_vector(data_width-1 downto 0) := (others => '0');
     signal to_clr_data_in, to_inc_data_in : std_logic;
-    signal ram_wr : std_logic;
+    signal ram_wr, sort_done : std_logic;
 ----------------------------------
 begin
-    uut : sort_top port map 
-            ( clk=>clk, rst=>rst, 
-              to_clr_data_in=>to_clr_data_in,
-              to_inc_data_in=>to_inc_data_in,
-              ram_wr=>ram_wr,
-              --unsrtd_data=>unsrtd_data,
-              srtd_data=>srtd_data );
+    uut : main_top port map 
+            ( clk => clk,
+              rst => rst, 
+              to_clr_data_in => to_clr_data_in,
+              to_inc_data_in => to_inc_data_in,
+              ram_wr => ram_wr );
 
     clk_proc : process begin
         clk <= '0';
@@ -68,7 +62,7 @@ begin
         wait for clk_period;
         to_inc_data_in <='1'; -- 7
         wait for clk_period;
-        to_inc_data_in <='1'; -- 8
+         to_inc_data_in <='1'; -- 8
         wait for clk_period;
         to_inc_data_in <='1'; -- 9
         wait for clk_period;
@@ -82,18 +76,13 @@ begin
         wait for clk_period;
         to_inc_data_in <='1'; -- 14
         wait for clk_period;
-        to_inc_data_in <='1';
-        wait for clk_period; 
-        wait for clk_period; 
+        ram_wr <= '1';
         wait for clk_period; 
         ram_wr <= '1';
-        wait for clk_period;
+        wait for clk_period; 
         ram_wr <= '1';
-        wait for clk_period;
+        wait for clk_period; 
         ram_wr <= '1';
-        wait for clk_period;
-        ram_wr <= '1';
-        wait for clk_period;
         wait;
     end process;
 end;
